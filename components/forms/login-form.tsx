8193dfb1bdde41cdacb804a29ae33953a3ee4565
BlackSquirrel <Maexxla3458@proton.me>
2026-01-12 14:59:30 +0100

    redirect to dashboard upon login

--- components/forms/login-form.tsx
+++ components/forms/login-form.tsx
@@ -34,6 +34,7 @@ import { signInUser } from "@/server/users";
 import { useState } from "react";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   email: z.email(),
@@ -44,6 +45,7 @@ export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -60,7 +62,7 @@ export function LoginForm({
       const response = await signInUser(values.email, values.password);
       if (response.success) {
         toast.success(response.message);
-        // router.push("/dashboard");
+        router.push("/dashboard");
       } else {
         toast.error(response.message);
       }
