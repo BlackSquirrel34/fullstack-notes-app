659f892287268dbbabcbeadfdb50f93a8718e921
BlackSquirrel <Maexxla3458@proton.me>
2026-01-13 11:48:05 +0100

    create new notebook and save to db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,26 @@
+import { CreateNotebookButton } from "@/components/create-notebook-button";
+import { Notebooks } from "@/components/notebooks";
 import { PageWrapper } from "@/components/page-wrapper";
+import { getNotebooks } from "@/server/notebooks";
 
-export default function Page() {
+export default async function Page() {
+  const notebooks = await getNotebooks();
+
   return (
     <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]}>
-      <h1>Dashboard Home</h1>
+      <h1>Notebooks</h1>
+      <CreateNotebookButton />
+      <div /* className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"*/
+      >
+        {notebooks.success &&
+          notebooks?.notebooks?.map((notebook) => (
+            <div key={notebook.id}>{notebook.name}</div>
+          ))}
+      </div>
+
+      {notebooks.success && notebooks?.notebooks?.length === 0 && (
+        <div>No notebooks found</div>
+      )}
     </PageWrapper>
   );
 }
---
+++ components/notebooks.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Notebooks = () => {
+  return <div>Notebooks</div>;
+};
